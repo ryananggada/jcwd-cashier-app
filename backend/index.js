a9526f9c8d5b09da9c107877e9a0de16a1756c66
ryananggada <khoryananggada@gmail.com>
2023-10-07 17:33:44 +0700

    Add backend

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+// const myRouter = require("./routes/myRoute");
+// app.use("/myRoute", myRouter);
+
+// 404 Not Found route
+app.use((req, res) => {
+  console.error(`Not found: ${req.path}`);
+  res.status(404).json({
+    ok: false,
+    message: "Route not found",
+  });
+});
+
+// Middleware error
+app.use((err, req, res, next) => {
+  console.error(`FATAL ERROR: ${req.path}`);
+  console.error(err);
+
+  res.status(500).json({
+    ok: false,
+    message: "FATAL ERROR",
+    error: String(err),
+  });
+});
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Cashier app server runs at http://localhost:${PORT}`);
+});
