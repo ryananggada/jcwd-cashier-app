ee22599c74248db5422af64ceb49c6fbe2e2489a
H4zardZ1 <101616102+H4zardZ1@users.noreply.github.com>
2023-10-11 10:50:44 +0700

    fix product on category dependence

--- backend/models/product.js
+++ backend/models/product.js
@@ -3,12 +3,10 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Product extends Model {
     static associate(models) {
-      /*
-      Product.belongsTo(models.category, {
+      Product.belongsTo(models.Category, {
         foreignKey: "category",
         as: "categoryData",
       });
-      */
     }
   }
   Product.init(
